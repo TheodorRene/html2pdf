cdf53269b892063e4f04d94d61d61f5f9f219ed8
Theodor René Carlsen <theodor.rene@gmail.com>
2023-01-26 19:52:42 +0100

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import { Page } from "./Page";
+import { Page, PageWithoutPadding } from "./Page";
 
 function App() {
   const [count, setCount] = useState(0);
--- src/Page.tsx
+++ src/Page.tsx
@@ -1,11 +1,11 @@
-import React, { ReactNode, PropsWithChildren } from "react";
+import { PropsWithChildren } from "react";
 
 export const Page = ({ children }: PropsWithChildren<{}>) => {
   return (
     <div
       className="
                 w-[210mm]
-                h-[295mm]
+                h-[297mm]
                 border-2
                 border-black
                 bg-white
@@ -16,6 +16,32 @@ export const Page = ({ children }: PropsWithChildren<{}>) => {
                 py-10
                 print:px-10
                 print:py-5
+                print:border-none
+                print:shadow-none
+                print:ml-0
+                print:mt-0
+                "
+    >
+      {children}
+    </div>
+  );
+};
+export const PageWithoutPadding = ({ children }: PropsWithChildren<{}>) => {
+  return (
+    <div
+      className="
+                w-[210mm]
+                h-[297mm]
+                border-2
+                border-black
+                bg-white
+                shadow-2xl
+                ml-2
+                mt-2
+                px-20
+                py-10
+                print:px-0
+                print:py-0
                 print:border-none
                 print:shadow-none
                 print:ml-0
